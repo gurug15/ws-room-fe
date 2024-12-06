881e9d0091b5df150b8f06f4361846b09076edd7
GURUGASTGAR <gurugastgar@gmail.com>
2024-12-07 00:55:57 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className=" flex justify-center items-center gap-2 w-screen h-screen bg-black text-white">
             <ChatBox/>
-            <ChatBox/>
+            {/* <ChatBox/> */}
         </div>
     );
 }
